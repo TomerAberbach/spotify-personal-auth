7e07f812e419601d7f700fa429fa2459fa8a3026
TomerADev <tomeraberbach@gmail.com>
2018-04-29 11:48:41 -0400

    Initial commit

---
+++ test.js
@@ -0,0 +1,26 @@
+const spotifyPersonalAuth = require('./index'),
+      SpotifyWebApi = require('spotify-web-api-node');
+
+// Configure module
+spotifyPersonalAuth.config({
+    clientId : 'YOUR_CLIENT_ID', // Replace with your client id
+    clientSecret : 'YOUR_CLIENT_SECRET', // Replace with your client secret
+    scope : ['user-modify-playback-state', 'user-top-read'], // Replace with your array of needed Spotify scopes
+    path : '/path/to/a/tokens.json' // Optional path to file to save tokens (will be created for you)
+});
+
+const api = new SpotifyWebApi();
+
+// Get token promise (call this again later if the token expires, it will refresh if needed)
+spotifyPersonalAuth.token().then(token => {
+    // Sets api access token
+    api.setAccessToken(token);
+
+    // Promise for user's top tracks
+    return api.getMyTopTracks();
+}).then(data =>
+    // Plays user's top tracks
+    api.play({
+        uris: data['body']['items'].map(item => item['uri'])
+    })
+).catch(console.log);
